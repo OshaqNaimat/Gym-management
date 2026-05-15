9186845e8fb5c8276726e1f1a1fc4127cb2addc8
Oshaq Naimat <oshaqnaimat15@gmail.com>
2026-05-15 19:09:33 +0500

    completed

--- GMS/resources/views/components/member-navbar.blade.php
+++ GMS/resources/views/components/member-navbar.blade.php
@@ -6,10 +6,10 @@
                 <i class="fa fa-bars"></i>
             </button>
             <div class="topbar-title" id="page-title">Dashboard</div>
-            <div class="topbar-search">
+            {{-- <div class="topbar-search">
                 <i class="fa fa-magnifying-glass"></i>
                 <input type="text" placeholder="Search...">
-            </div>
+            </div> --}}
             <div class="topbar-actions">
                 <!-- <div class="icon-btn" onclick="showPage('announcements')"><i class="fa fa-bell"></i><div class="badge-dot"></div></div> -->
                 <div class="icon-btn" id="themeToggle" onclick="toggleTheme()" title="Toggle Theme">
